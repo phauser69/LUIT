8e05439e8c9dcccbb7ef94bed79f4ebf1f1d3d0d
Pierre Hauser <pierrephauser@gmail.com>
2024-12-09 16:50:26 -0500

    Python Project wk1

---
+++ EC2 Random Name Generator.py
@@ -0,0 +1,5 @@
+Department=['Marketing','Accounting','FinOps']
+EC2cnt = int(input("Please enter how many EC2's that require a name "))
+Dprtnm = input("What is your Department name? ")
+print ("Okay you will receive ")+ EC2cnt +(" Names")
+
